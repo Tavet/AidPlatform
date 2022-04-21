c7023939aab90fd745a12b8f244ff279000674e4
Breyner Rojas <brojas22@outlook.com>
2022-04-21 13:28:20 -0500

    Create boilerplate based on Quarkus started project

---
+++ services/boilerplate/src/test/java/io/tavet/aid/boilerplate/GreetingResourceIT.java
@@ -0,0 +1,9 @@
+package io.tavet.aid.boilerplate;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
