ada4c7a6658f41e02140d6c44459c21a4d49fa63
Breyner Rojas <brojas22@outlook.com>
2022-04-25 18:25:13 -0500

    Enable contexts and dependency injection (CDI) to inject domain use cases in the app

--- services/service_boilerplate/app/src/main/java/io/tavet/aid/app/rest/AidResource.java
+++ services/service_boilerplate/app/src/main/java/io/tavet/aid/app/rest/AidResource.java
@@ -2,7 +2,6 @@ package io.tavet.aid.app.rest;
 
 import java.util.UUID;
 
-import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -11,29 +10,27 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import io.quarkus.arc.runtime.BeanContainer.Instance;
 import io.tavet.aid.app.request.CreateAidRequest;
 import io.tavet.aid.app.response.CreateAidResponse;
 import io.tavet.aid.domain.Aid;
 import io.tavet.aid.domain.service.AidService;
 
 @Path("/aid")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class AidResource {
 
-    // @Inject
-    // private Instance<AidService> aidService;
+    @Inject
+    private AidService aidService;
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
     public Aid hello() {
         return new Aid();
     }
 
     @POST
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
     public CreateAidResponse createAidRequest(CreateAidRequest request) {
-        // final UUID id = aidService.get().createAidRequest(request.getAid());
-        return new CreateAidResponse(UUID.randomUUID());
+        final UUID id = aidService.createAidRequest(request.getAid());
+        return new CreateAidResponse(id);
     }
 }
--- services/service_boilerplate/domain/src/main/java/io/tavet/aid/domain/service/DomainAidService.java
+++ services/service_boilerplate/domain/src/main/java/io/tavet/aid/domain/service/DomainAidService.java
@@ -3,13 +3,8 @@ package io.tavet.aid.domain.service;
 import java.util.UUID;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.context.Dependent;
-import javax.inject.Inject;
-import javax.inject.Singleton;
 
-import io.quarkus.arc.DefaultBean;
 import io.tavet.aid.domain.Aid;
-import io.tavet.aid.domain.repository.AidRepository;
 
 @ApplicationScoped
 public class DomainAidService implements AidService {
