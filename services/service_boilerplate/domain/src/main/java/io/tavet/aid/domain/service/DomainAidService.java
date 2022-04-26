1f8e763f770cadc4ce36ec90e22d8e9b79430857
Breyner Rojas <brojas22@outlook.com>
2022-04-26 14:03:25 -0500

    Fix dependency injection from the infrastructure module

--- services/service_boilerplate/domain/src/main/java/io/tavet/aid/domain/service/DomainAidService.java
+++ services/service_boilerplate/domain/src/main/java/io/tavet/aid/domain/service/DomainAidService.java
@@ -3,17 +3,17 @@ package io.tavet.aid.domain.service;
 import java.util.UUID;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
 
-import io.quarkus.arc.runtime.BeanContainer.Instance;
 import io.tavet.aid.domain.Aid;
 import io.tavet.aid.domain.repository.AidRepository;
 
 @ApplicationScoped
 public class DomainAidService implements AidService {
 
-    // @Inject
-    // private Instance<AidRepository> aidRepository;
+    @Inject
+    private Instance<AidRepository> aidRepository;
 
     @Override
     public UUID createAidRequest(Aid aid) {
@@ -24,8 +24,7 @@ public class DomainAidService implements AidService {
 
     @Override
     public Aid getAidRequest(UUID id) {
-        // return aidRepository.findById(id);
-        return null;
+        return aidRepository.get().findById(id);
     }
 
 }
