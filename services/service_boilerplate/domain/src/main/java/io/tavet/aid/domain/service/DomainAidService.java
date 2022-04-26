be4462509efaf21baf23437642313daffc548930
Breyner Rojas <brojas22@outlook.com>
2022-04-26 13:56:41 -0500

    Add Flyway migration and initial script

--- services/service_boilerplate/domain/src/main/java/io/tavet/aid/domain/service/DomainAidService.java
+++ services/service_boilerplate/domain/src/main/java/io/tavet/aid/domain/service/DomainAidService.java
@@ -5,14 +5,15 @@ import java.util.UUID;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
+import io.quarkus.arc.runtime.BeanContainer.Instance;
 import io.tavet.aid.domain.Aid;
 import io.tavet.aid.domain.repository.AidRepository;
 
 @ApplicationScoped
 public class DomainAidService implements AidService {
 
-    @Inject
-    private AidRepository aidRepository;
+    // @Inject
+    // private Instance<AidRepository> aidRepository;
 
     @Override
     public UUID createAidRequest(Aid aid) {
@@ -23,7 +24,8 @@ public class DomainAidService implements AidService {
 
     @Override
     public Aid getAidRequest(UUID id) {
-        return aidRepository.findById(id);
+        // return aidRepository.findById(id);
+        return null;
     }
 
 }
