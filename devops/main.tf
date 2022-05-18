7c55c978205a7dd73502fd2f235b6bdd9b0e9780
Breyner Rojas <brojas22@outlook.com>
2022-05-18 13:00:40 -0500

    Create the nat and internet gateways for private/public routes

--- devops/main.tf
+++ devops/main.tf
@@ -14,12 +14,14 @@ provider "aws" {
   region  = "eu-central-1"
 }
 
+// VPC
+
 resource "aws_vpc" "aid_platform" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
-    Name                                = "aidplatform_vpc"
+    Name                                = "aidplatform-vpc"
     Project                             = "AidPlatform"
     "kubernetes.io/cluster/aidplatform" = "shared"
   }
@@ -52,16 +54,71 @@ resource "aws_subnet" "aid_platform_public" {
   }
 }
 
-resource "aws_route_table" "aid_platform" {
+// Internet gateway
+resource "aws_internet_gateway" "aid_platform_gw" {
   vpc_id = aws_vpc.aid_platform.id
-  route  = []
+
   tags = {
-    Name    = "aidplatform_routetable"
+    Name    = "aidplatform-"
     Project = "AidPlatform"
   }
+}
+
+// Default Route Table
+resource "aws_default_route_table" "aid_platform" {
+  default_route_table_id = aws_vpc.aid_platform.default_route_table_id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.aid_platform.id
+  }
+
+  tags = {
+    Name = "example"
+  }
+
+  depends_on = [aws_nat_gateway.aid_platform]
+}
+
+// Public Route table
+
+resource "aws_route_table" "aid_platform" {
+  vpc_id = aws_vpc.aid_platform.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.aid_platform_gw.id
+  }
+  tags = {
+    Name    = "aidplatform-routetable"
+    Project = "AidPlatform"
+  }
+
+  depends_on = [aws_internet_gateway.aid_platform_gw]
 }
 
 resource "aws_route_table_association" "aid_platform_public" {
   subnet_id      = aws_subnet.aid_platform_public.id
   route_table_id = aws_route_table.aid_platform.id
 }
+
+// EIP
+
+resource "aws_eip" "aid_platform" {
+  vpc = true
+}
+
+// NAT
+resource "aws_nat_gateway" "aid_platform" {
+  allocation_id = aws_eip.aid_platform.id
+  subnet_id     = aws_subnet.aid_platform_public.id
+
+  tags = {
+    Name    = "aidplatform-routetable"
+    Project = "AidPlatform"
+  }
+
+  depends_on = [aws_internet_gateway.aid_platform_gw]
+}
+
+
